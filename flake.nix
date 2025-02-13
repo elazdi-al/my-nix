31132bbcf768425477e36ee166b09fb473b2030e
elazdi-al <alielazdi@gmail.com>
2025-02-13 22:16:58 +0100

    settings: autohide dock

--- flake.nix
+++ flake.nix
@@ -55,6 +55,9 @@
 
       # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
+      
+      #Mac Settings
+      system.defaults.dock.autohide = true;
     };
   in
   {
